5ca5132b231310af3f252c133eb96c827d8c31d0
Carson Ray <carsonray314@gmail.com>
2023-08-02 13:52:19 -0400

    Started audio encoding testing

--- tests/gps_audio_encoder/gps_audio_encoder.ino
+++ tests/gps_audio_encoder/gps_audio_encoder.ino
@@ -3,7 +3,7 @@
 #include <GPSTimer.h>
 
 /*
-  Demonstrates microsecond-accuracy timing using GPS module
+  Encodes GPS time into square wave audio tone
 */
 
 //Serial settings
@@ -13,60 +13,33 @@
 //PPS pin
 #define ppsPin 2
 
+//Tone pin
+#define tonePin 5
+
 //Serial connection object
-SoftwareSerial ss(Rx, Tx);
+//SoftwareSerial ss(Rx, Tx);
 
 //TinyGPSPlus object
-TinyGPSPlus gps;
+//TinyGPSPlus gps;
 
 //GPS timer object
-GPSTimer timer = GPSTimer(&gps);
+//GPSTimer timer = GPSTimer(&gps);
 
 void setup() {
-  Serial.begin(115200);
-  ss.begin(9600);
+  //Serial.begin(115200);
+  //ss.begin(9600);
 
-  timer.attachPPS(ppsPin);
+  //timer.attachPPS(ppsPin);
+  tone(tonePin, 2000);
 }
 
 void loop() {
-  //Prints number of satellites
-  Serial.print("Satellites: ");
-  Serial.println(gps.satellites.value());
-
-  //Prints current date if valid
-  Serial.print("Date: ");
-  if (gps.date.isValid()) {
-    char buffer[32];
-    sprintf(buffer, "%02d/%02d/%02d", timer.month(), timer.day(), timer.year());
-    Serial.println(buffer);
-  } else {
-    Serial.println();
-  }
-
-  //Prints current time if valid
-  Serial.print("Time: ");
-  if (gps.time.isValid()) {
-    char buffer[32];
-    sprintf(buffer, "%02d:%02d:%02d:%06lu", timer.hour(), timer.minute(), timer.second(), timer.microsecond());
-    Serial.println(buffer);
-  } else {
-    Serial.println();
-  }
-  Serial.print("us per Second: ");
-  Serial.println(timer.getMicrosPerSecond());
-  Serial.print("Second Error: ");
-  Serial.println(timer.getSecondError());
-
-  //Notifies if GPS data not recieved
-  if (millis() > 500 && gps.charsProcessed() < 10)
-    Serial.println("No GPS data received");
-
+  
   //Delays and updates
-  updateDelay(500);
+  //updateDelay(500);
 }
 
-static void updateDelay(uint32_t ms)
+/*static void updateDelay(uint32_t ms)
 {
   uint32_t start = millis();
   do
@@ -78,4 +51,4 @@ static void updateDelay(uint32_t ms)
       //Updates timer object
       timer.update();
   } while (millis() - start < ms);
-}
+}*/
